b504e60bd8a0e7c11d4fb1391194bc57c192103c
antonsynd <nguyen.antony@gmail.com>
2025-03-16 23:33:48 -0400

    Wrapper: Add basic wrapper class

---
+++ dotnet/src/Sharpy.Tests/Wrapper.cs
@@ -0,0 +1,18 @@
+namespace Sharpy.Tests {
+    public class Wrapper<T>(T value)
+    {
+        private static uint _id;
+
+        public readonly uint Id = _id++;
+        public readonly T Value = value;
+
+        // public bool Equals(Wrapper<T> other)
+        // {
+        //     if (Value is null) {
+        //         return other.Value is null && Id == other.Id;
+        //     }
+
+        //     return Value.Equals(other.Value);
+        // }
+    }
+}
