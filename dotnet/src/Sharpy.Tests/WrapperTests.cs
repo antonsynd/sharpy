887c7d75cd0a2760691af1982d95b4752e22f237
antonsynd <nguyen.antony@gmail.com>
2025-03-27 22:32:27 -0700

    tests: Add more Wrapper<T> tests

--- dotnet/src/Sharpy.Tests/WrapperTests.cs
+++ dotnet/src/Sharpy.Tests/WrapperTests.cs
@@ -71,6 +71,7 @@ namespace Sharpy.Tests
             // When/then
             wrapper0.__Eq__(wrapper1_0).Should().BeFalse();
             wrapper0.__Eq__(wrapper1_1).Should().BeFalse();
+            wrapper0.__Eq__(null).Should().BeFalse();
 
             // Identity
             wrapper0.__Eq__(wrapper0).Should().BeTrue();
@@ -80,6 +81,103 @@ namespace Sharpy.Tests
             // Symmetric
             wrapper1_0.__Eq__(wrapper1_1).Should().BeTrue();
             wrapper1_1.__Eq__(wrapper1_0).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Wrapper_Inequatable()
+        {
+            // If
+            typeof(Wrapper<int>).IsAssignableTo(typeof(Inequatable<Object>)).Should().BeTrue();
+            typeof(Wrapper<int>).IsAssignableTo(typeof(Inequatable<Wrapper<int>>)).Should().BeTrue();
+            var wrapper0 = new Wrapper<int>(0);
+            var wrapper1_0 = new Wrapper<int>(1);
+            var wrapper1_1 = new Wrapper<int>(1);
+
+            wrapper1_0.__Hash__().Should().NotBe(wrapper1_1.__Hash__());
+
+            // When/then
+            wrapper0.__Ne__(wrapper1_0).Should().BeTrue();
+            wrapper0.__Ne__(wrapper1_1).Should().BeTrue();
+            wrapper0.__Ne__(null).Should().BeTrue();
+
+            // Identity
+            wrapper0.__Ne__(wrapper0).Should().BeFalse();
+            wrapper1_0.__Ne__(wrapper1_0).Should().BeFalse();
+            wrapper1_1.__Ne__(wrapper1_1).Should().BeFalse();
+
+            // Symmetric
+            wrapper1_0.__Ne__(wrapper1_1).Should().BeFalse();
+            wrapper1_1.__Ne__(wrapper1_0).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Wrapper_IEquatable()
+        {
+            // If
+            typeof(Wrapper<int>).IsAssignableTo(typeof(IEquatable<Object>)).Should().BeTrue();
+            typeof(Wrapper<int>).IsAssignableTo(typeof(IEquatable<Wrapper<int>>)).Should().BeTrue();
+
+            var wrapper0 = new Wrapper<int>(0);
+            var wrapper1_0 = new Wrapper<int>(1);
+            var wrapper1_1 = new Wrapper<int>(1);
+
+            wrapper1_0.__Hash__().Should().NotBe(wrapper1_1.__Hash__());
+
+            // When/then
+            wrapper0.Equals(wrapper1_0).Should().BeFalse();
+            wrapper0.Equals(wrapper1_1).Should().BeFalse();
+            wrapper0.Equals(null).Should().BeFalse();
+
+
+            // Identity
+            wrapper0.Equals(wrapper0).Should().BeTrue();
+            wrapper1_0.Equals(wrapper1_0).Should().BeTrue();
+            wrapper1_1.Equals(wrapper1_1).Should().BeTrue();
+
+            // Symmetric
+            wrapper1_0.Equals(wrapper1_1).Should().BeTrue();
+            wrapper1_1.Equals(wrapper1_0).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Wrapper_Identifiable()
+        {
+            // If
+            typeof(Wrapper<int>).IsAssignableTo(typeof(Identifiable)).Should().BeTrue();
+            var wrapper0 = new Wrapper<int>(0);
+            var wrapper1 = new Wrapper<int>(1);
+
+            // When/then
+            wrapper0.__Id__().Should().NotBe(wrapper0.__Hash__());
+            wrapper1.__Id__().Should().NotBe(wrapper1.__Hash__());
+            wrapper0.__Id__().Should().NotBe(wrapper1.__Id__());
+        }
+
+        [Fact]
+        public void Wrapper_Representable()
+        {
+            // If
+            typeof(Wrapper<int>).IsAssignableTo(typeof(Representable)).Should().BeTrue();
+            var wrapper0 = new Wrapper<int>(0);
+            var wrapper1 = new Wrapper<int>(1);
+
+            // When/then
+            wrapper0.__Repr__().Length.Should().BeGreaterThan(0);
+            wrapper0.__Repr__().Should().NotBe(wrapper1.__Repr__());
+        }
+
+        [Fact]
+        public void Wrapper_StrConvertible()
+        {
+            // If
+            typeof(Wrapper<int>).IsAssignableTo(typeof(StrConvertible)).Should().BeTrue();
+            var wrapper0 = new Wrapper<int>(0);
+            var wrapper1 = new Wrapper<int>(1);
+
+            // When/then
+            wrapper0.__Str__().Should().Be(wrapper0.__Repr__());
+            wrapper0.ToString().Should().Be(wrapper0.__Str__());
+            wrapper0.__Str__().Should().NotBe(wrapper1.__Str__());
         }
     }
 }
