a7554b83c86af3113b8b0397b954b75a6b664ed4
antonsynd <nguyen.antony@gmail.com>
2025-04-13 22:37:41 -0400

    Add: Fix Add operator

--- dotnet/src/Sharpy/Operator/Add.cs
+++ dotnet/src/Sharpy/Operator/Add.cs
@@ -17,5 +17,9 @@ public static partial class Exports
 
     public static T __Add__<T>(T left, T right) where T : IAddable<T> => Add<T>(left, right);
 
-    public static TSum __Add__<TLeft, TRight, TSum>(TLeft left, TRight right) => Add(left, right);
+    public static TSum __Add__<TLeft, TRight, TSum>(TLeft left, TRight right)
+        where TLeft : IAddable<TLeft, TRight, TSum>
+        where TRight : IAddable<TLeft, TRight, TSum>
+        where TSum : IAddable<TLeft, TRight, TSum>
+        => Add<TLeft, TRight, TSum>(left, right);
 }
