3fc0891318c1c3034e80ac05cf305bb65756302f
antonsynd <nguyen.antony@gmail.com>
2025-04-14 23:21:32 -0400

    is: Rename from is_

--- dotnet/src/Sharpy/Operator/Is.cs
+++ dotnet/src/Sharpy/Operator/Is.cs
@@ -2,7 +2,7 @@ namespace Sharpy.Operator;
 
 public static partial class Exports
 {
-    public static bool Is_(object left, object right)
+    public static bool Is(object left, object right)
     {
         return ReferenceEquals(left, right);
     }
