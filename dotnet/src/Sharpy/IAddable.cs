b3eb89d7f2c6bd323ee553829e78443e9b9b5c12
antonsynd <nguyen.antony@gmail.com>
2025-04-15 08:36:39 -0400

    inplace: Add inplace ops

--- dotnet/src/Sharpy/IAddable.cs
+++ dotnet/src/Sharpy/IAddable.cs
@@ -1,9 +1,6 @@
 namespace Sharpy;
 
-public interface IAddable<TLeft, TRight, TSum>
-    where TLeft : IAddable<TLeft, TRight, TSum>
-    where TRight : IAddable<TLeft, TRight, TSum>
-    where TSum : IAddable<TLeft, TRight, TSum>
+public interface IAddable<TLeft, TRight, TSum> where TLeft : IAddable<TLeft, TRight, TSum>
 {
     TSum __Add__(TRight other);
 
@@ -25,8 +22,7 @@ public interface IAddable<TLeft, TRight, TSum>
 /// </summary>
 /// <typeparam name="TAugend">The type that gets augmented (the "augend").</typeparam>
 /// <typeparam name="TAddend">The type that indicates the amount of augmentation or what is being included in the "augend".</typeparam>
-public interface IAddable<TAugend, TAddend>
-    where TAugend : IAddable<TAugend, TAddend>
+public interface IAddable<TAugend, TAddend> where TAugend : IAddable<TAugend, TAddend>
 {
     TAugend __Add__(TAddend other);
 
