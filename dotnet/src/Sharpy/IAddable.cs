286260da1e3f6efb08d9a9acfb1d37e1e97fc6be
antonsynd <nguyen.antony@gmail.com>
2025-04-18 08:16:49 -0400

    addable/multipliable: Add *With<> interfaces

--- dotnet/src/Sharpy/IAddable.cs
+++ dotnet/src/Sharpy/IAddable.cs
@@ -1,10 +1,32 @@
 namespace Sharpy;
 
-public interface IAddable<TLeft, TRight, TSum> where TLeft : IAddable<TLeft, TRight, TSum>
+/// <summary>
+/// An interface for a type (the augend) that can be augmented (or added to)
+/// with something (the addende) producing a sum (the result, which may be
+/// the modified augend itself.
+/// </summary>
+/// <typeparam name="TAddend">The addend (the increase or thing to
+/// add/insert).</typeparam>
+/// <typeparam name="TSum">The sum.</typeparam>
+public interface IAddableWith<TAddend, TSum>
 {
-    TSum __Add__(TRight other);
+    TSum __Add__(TAddend other);
+}
 
-    static virtual TSum operator +(TLeft left, TRight right)
+/// <summary>
+/// An interface for a type (the augend) that can be augmented (or added to)
+/// with something (the addende) producing a sum (the result, which may be
+/// the modified augend itself. This version has native operator support.
+/// </summary>
+/// <typeparam name="TAugend">The augend (the thing to add something to, or the
+/// thing that is increased or augmented).</typeparam>
+/// <typeparam name="TAddend">The addend (the increase or thing to
+/// add/insert).</typeparam>
+/// <typeparam name="TSum">The sum.</typeparam>
+public interface IAddable<TAugend, TAddend, TSum> : IAddableWith<TAddend, TSum>
+    where TAugend : IAddable<TAugend, TAddend, TSum>
+{
+    static virtual TSum operator +(TAugend left, TAddend right)
     {
         if (left is null || right is null)
         {
@@ -20,8 +42,10 @@ public interface IAddable<TLeft, TRight, TSum> where TLeft : IAddable<TLeft, TRi
 /// is the element to be augmented (increased) and the right operand is the
 /// amount of augumentation or something to be included in the other element.
 /// </summary>
-/// <typeparam name="TAugend">The type that gets augmented (the "augend").</typeparam>
-/// <typeparam name="TAddend">The type that indicates the amount of augmentation or what is being included in the "augend".</typeparam>
+/// <typeparam name="TAugend">The type that gets augmented
+/// (the "augend").</typeparam>
+/// <typeparam name="TAddend">The type that indicates the amount of
+/// augmentation or what is being included in the "augend".</typeparam>
 public interface IAddable<TAugend, TAddend> where TAugend : IAddable<TAugend, TAddend>
 {
     TAugend __Add__(TAddend other);
@@ -37,6 +61,10 @@ public interface IAddable<TAugend, TAddend> where TAugend : IAddable<TAugend, TA
     }
 }
 
+/// <summary>
+/// An interface for a type that can be added to itself yielding a result
+/// of the same type.
+/// </summary>
 public interface IAddable<T> : IAddable<T, T, T> where T : IAddable<T, T, T>
 {
 }
--- dotnet/src/Sharpy/IMultipliable.cs
+++ dotnet/src/Sharpy/IMultipliable.cs
@@ -1,10 +1,28 @@
 namespace Sharpy;
 
-public interface IMultipliable<TMultiplicand, TMultiplier, TProduct>
-    where TMultiplicand : IMultipliable<TMultiplicand, TMultiplier, TProduct>
+/// <summary>
+/// An interface for a type (the multiplicand) that can be multiplied with
+/// something (the multiplier) producing a result (the product).
+/// </summary>
+/// <typeparam name="TMultiplier">The multiplier (the factor).</typeparam>
+/// <typeparam name="TProduct">The product.</typeparam>
+public interface IMultipliableWith<TMultiplier, TProduct>
 {
     TProduct __Mul__(TMultiplier other);
+}
 
+/// <summary>
+/// An interface for a type (the multiplicand) that can be multiplied with
+/// something (the multiplier) producing a result (the product) with native
+/// operator support.
+/// </summary>
+/// <typeparam name="TMultiplicannd">The multiplicand (what is being
+/// multiplied).</typeparam>
+/// <typeparam name="TMultiplier">The multiplier (i.e. the factor).</typeparam>
+/// <typeparam name="TProduct">The product.</typeparam>
+public interface IMultipliable<TMultiplicand, TMultiplier, TProduct> : IMultipliableWith<TMultiplier, TProduct>
+    where TMultiplicand : IMultipliable<TMultiplicand, TMultiplier, TProduct>
+{
     static virtual TProduct operator *(TMultiplicand left, TMultiplier right)
     {
         if (left is null || right is null)
@@ -16,11 +34,23 @@ public interface IMultipliable<TMultiplicand, TMultiplier, TProduct>
     }
 }
 
+/// <summary>
+/// An interface for a type (the multiplicand) that can be multiplied with
+/// something (the multiplier) producing a result (the product) with native
+/// operator support, where the result is of the same type as the multiplicand.
+/// </summary>
+/// <typeparam name="TMultiplicannd">The multiplicand (what is being
+/// multiplied), and also the type of the product (the result).</typeparam>
+/// <typeparam name="TMultiplier">The multiplier (i.e. the factor).</typeparam>
 public interface IMultipliable<TMultiplicand, TMultipilier> : IMultipliable<TMultiplicand, TMultipilier, TMultiplicand>
     where TMultiplicand : IMultipliable<TMultiplicand, TMultipilier, TMultiplicand>
 {
 }
 
+/// <summary>
+/// An interface for a type that can be multiplied by itself yielding a result
+/// of the same type.
+/// </summary>
 public interface IMultipliable<T> : IMultipliable<T, T, T> where T : IMultipliable<T, T, T>
 {
 }
