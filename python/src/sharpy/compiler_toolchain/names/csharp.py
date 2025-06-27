a935c3ffd11b571d50ecff15c8422081b40ea157
antonsynd <nguyen.antony@gmail.com>
2025-06-26 23:30:45 -0400

    csharp: Further the model for C# assemblies

--- python/src/sharpy/compiler_toolchain/names/csharp.py
+++ python/src/sharpy/compiler_toolchain/names/csharp.py
@@ -1,14 +1,67 @@
-from typing import MutableSet, Self, Sequence
+from typing import Mapping, MutableMapping, MutableSet, Self, Set, Sequence, Optional
 
 
-class Type:
-    """Represents a C# type in the compiler toolchain."""
+class _TypeBase:
+    pass
 
-    def __init__(self, name: str) -> None:
+
+class _NamespaceBase:
+    """
+    Base class for namespaces in the C# compiler toolchain. This is used to
+    provide a common interface for the Namespace class.
+    """
+
+    def name(self) -> str:
+        raise NotImplementedError
+
+    def parent(self) -> Optional[Self]:
+        raise NotImplementedError
+
+    def namespaces(self) -> Mapping[str, Self]:
+        raise NotImplementedError
+
+    def symbols(self):
+        raise NotImplementedError
+
+
+class Type(_TypeBase):
+    """
+    Represents a C# type in the compiler toolchain. It can have fields
+    which are actual members (instance or static) or nested types. C#
+    requires that all names under a type are unique, which makes this easier
+    to model.
+    """
+
+    def __init__(
+        self, name: str, superclass: Optional[Self] = None, interfaces: Optional[Set[Self]] = None
+    ) -> None:
         self._name: str = name
+        self._namespace: _NamespaceBase = None  # This will be set by the Namespace class
+        self._fields: MutableMapping[str, Self] = dict()
+        # Note: This requires Python 3.10+ for set() to be ordered
+        self._interfaces: MutableSet[Self] = interfaces if interfaces is not None else set()
+        self._superclass: Optional[Self] = superclass
 
     def name(self) -> str:
         return self._name
+
+    def namespace(self) -> _NamespaceBase:
+        return self._namespace
+
+    def superclass(self) -> Optional[Self]:
+        return self._superclass
+
+    def interfaces(self) -> Set[Self]:
+        return set(self._interfaces)
+
+    def add_interface(self, interface: Self) -> None:
+        self._interfaces.add(interface)
+
+    def fields(self) -> MutableMapping[str, Self]:
+        return self._fields
+
+    def add_field(self, name: str, type_: Self) -> None:
+        self._fields[name] = type_
 
     def __eq__(self, value: object) -> bool:
         if not isinstance(value, Type):
@@ -20,194 +73,83 @@ class Type:
         return hash(self._name)
 
 
-class Member:
-    """Represents a member of a C# class or interface."""
-
-    def __init__(self, name: str, type_: Type) -> None:
-        self._name: str = name
-        self._type: Type = type_
-
-    def name(self) -> str:
-        return self._name
-
-    def type(self) -> Type:
-        return self._type
-
-    def __eq__(self, value: object) -> bool:
-        if not isinstance(value, Member):
-            return False
-
-        return self._name == value._name and self._type == value._type
-
-    def __hash__(self) -> int:
-        return hash((self._name, self._type))
-
-
-class Constructible(Type):
+class Symbols:
     """
-    Represents a constructible type in C# in the compiler toolchain. This
-    includes classes and structs.
+    Represents the symbols in a C# namespace in the compiler toolchain.
     """
 
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self._static: MutableSet[Member] = set()
-        self._instance: MutableSet[Member] = set()
+    def __init__(self, namespace: Namespace) -> None:
+        self._namespace: Namespace = namespace
+        self._types: MutableMapping[str, Type] = dict()
 
-    def add_static(self, member: Member) -> None:
-        self._static.add(member)
+    def add_type(self, type_: Type) -> None:
+        self._types[type_.name()] = type_
 
-    def add_instance(self, member: Member) -> None:
-        self._instance.add(member)
-
-    def static_members(self) -> MutableSet[Member]:
-        return self._static
-
-    def instance_members(self) -> MutableSet[Member]:
-        return self._instance
-
-    def __eq__(self, value: object) -> bool:
-        if not isinstance(value, Constructible):
-            return False
-
-        return (
-            self._name == value._name
-            and self._static == value._static
-            and self._instance == value._instance
-        )
-
-    def __hash__(self) -> int:
-        return hash((self._name, frozenset(self._static), frozenset(self._instance)))
-
-
-class Interface:
-    """Represents a C# interface in the compiler toolchain."""
-
-    def __init__(self, name: str) -> None:
-        self._name: str = name
-        self._methods: MutableSet[Member] = set()
-
-    def add_method(self, member: Member) -> None:
-        self._methods.add(member)
-
-    def methods(self) -> MutableSet[Member]:
-        return self._methods
-
-    def __eq__(self, value: object) -> bool:
-        if not isinstance(value, Interface):
-            return False
-
-        return self._name == value._name and self._methods == value._methods
-
-    def __hash__(self) -> int:
-        return hash((self._name, frozenset(self._methods)))
-
-
-class Enum(Type):
-    """Represents a C# enum in the compiler toolchain."""
-
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self._fields: MutableSet[str] = set()
-
-    def add_field(self, field: str) -> None:
-        self._fields.add(field)
-
-    def fields(self) -> MutableSet[str]:
-        return self._fields
-
-    def __eq__(self, value: object) -> bool:
-        if not isinstance(value, Enum):
-            return False
-
-        return self._name == value._name and self._fields == value._fields
-
-    def __hash__(self) -> int:
-        return hash((self._name, frozenset(self._fields)))
+    def types(self) -> Mapping[str, Type]:
+        return self._types
 
 
 class Namespace:
     """
-    Represents a C# namespace in the compiler toolchain. C# namespaces can only
-    contain classes, interfaces, and enums, so it makes it relatively easy to
-    model. The only difficulty is that C# classes can be static and hold
-    static members.
+    Represents a namespace in C# in the compiler toolchain. A namespace can
+    have any number of child namespaces and can contain symbols.
+    """
+
+    def __init__(self, name: str, parent: Optional[Self] = None) -> None:
+        self._name: str = name
+        self._parent: Optional[Self] = parent
+        self._symbols: Symbols = Symbols(self)
+        self._namespaces: MutableMapping[str, Self] = dict()
+
+    def name(self) -> str:
+        return self._name
+
+    def parent(self) -> Optional[Self]:
+        return self._parent
+
+    def add_namespace(self, namespace: Self) -> None:
+        self._namespaces[namespace.name()] = namespace
+        namespace._parent = self
+
+    def namespaces(self) -> Mapping[str, Self]:
+        return self._namespaces
+
+    def symbols(self) -> Symbols:
+        return self._symbols
+
+
+class Assembly:
+    """
+    Represents a C# assembly in the compiler toolchain. It contains a
+    hierarchy of namespaces.
     """
 
     def __init__(self, name: str) -> None:
         self._name: str = name
-        self._namespaces: MutableSet[str] = set()
-        self._constructibles: MutableSet[Constructible] = set()
-        self._interfaces: MutableSet[Interface] = set()
-        self._enums: MutableSet[Enum] = set()
-
-    def add_namespace(self, namespace: str) -> None:
-        # Note: To avoid complexity, we store nested namespaces as strings and
-        # maintain the hierarchy in a separate structure. Even though C#
-        # namespaces cannot have relative imports like Python modules can,
-        # Sharpy does allow this, so we need to have the hierarchy in place.
-        self._namespaces.add(namespace)
-
-    def add_constructible(self, constructible: Constructible) -> None:
-        self._constructibles.add(constructible)
-
-    def add_interface(self, interface: Interface) -> None:
-        self._interfaces.add(interface)
-
-    def add_enum(self, enum: Enum) -> None:
-        self._enums.add(enum)
+        self._root: Namespace = Namespace(name="<root>", parent=None)
 
     def name(self) -> str:
         return self._name
 
-    def namespaces(self) -> MutableSet[str]:
-        return self._namespaces
+    def namespaces(self) -> Mapping[str, Namespace]:
+        return self._root.namespaces()
 
-    def constructibles(self) -> MutableSet[Constructible]:
-        return self._constructibles
+    def add_namespace(self, namespace: Namespace, parents: Sequence[str]) -> None:
+        if not parents:
+            # If no parents, add the namespace directly
+            self._root.add_namespace(namespace)
 
-    def interfaces(self) -> MutableSet[Interface]:
-        return self._interfaces
+        else:
+            current_parent: Namespace = self._root
 
-    def enums(self) -> MutableSet[Enum]:
-        return self._enums
+            for parent in parents:
+                current_parent_opt: Optional[Namespace] = current_parent.namespaces().get(parent)
 
+                if current_parent_opt:
+                    current_parent = current_parent_opt
+                else:
+                    raise ValueError(
+                        f'Parent namespace "{parent}" in {'.'.join(parents)} not found.'
+                    )
 
-class NamespaceNode:
-    """Represents a node in a C# namespace hierarchy."""
-
-    def __init__(self, namespace: Namespace) -> None:
-        self._namespace: Namespace = namespace
-        self._nested_namespaces: MutableSet[Self] = set()
-
-    def add_namespace(self, namespace: Self) -> None:
-        self._nested_namespaces.add(namespace)
-
-    def namespace(self) -> Namespace:
-        return self._namespace
-
-    def nested_namespaces(self) -> MutableSet[Self]:
-        return self._nested_namespaces
-
-    def __hash__(self) -> int:
-        return hash(self._namespace.name())
-
-
-class DynamicLinkLibrary:
-    """Represents a C# dynamic link library (DLL) in the compiler toolchain."""
-
-    def __init__(self, name: str) -> None:
-        self._name: str = name
-        self._namespaces: MutableSet[NamespaceNode] = set()
-
-    def name(self) -> str:
-        return self._name
-
-    def namespaces(self) -> MutableSet[NamespaceNode]:
-        return self._namespaces
-
-    def add_top_level_namespace(self, namespace: Namespace) -> None:
-        self._namespaces.add(NamespaceNode(namespace))
-
-    def add_nested_namespace(self, namespace: Namespace, parents: Sequence[Namespace]) -> None:
-        pass
+            current_parent.add_namespace(namespace)
