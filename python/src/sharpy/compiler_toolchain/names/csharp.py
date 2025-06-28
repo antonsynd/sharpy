55d6c8fd770f5831bbb6f56d520332e7c0e8edec
antonsynd <nguyen.antony@gmail.com>
2025-06-27 23:06:36 -0400

    csharpy: Add AssemblyBuilder

--- python/src/sharpy/compiler_toolchain/names/csharp.py
+++ python/src/sharpy/compiler_toolchain/names/csharp.py
@@ -1,30 +1,41 @@
-from typing import Mapping, MutableMapping, MutableSet, Self, Set, Sequence, Optional
-
-
-class _TypeBase:
-    pass
+from typing import Mapping, MutableMapping, MutableSet, Optional, Self, Sequence, Set
 
 
 class _NamespaceBase:
     """
-    Base class for namespaces in the C# compiler toolchain. This is used to
-    provide a common interface for the Namespace class.
+    Abstract base class for namespaces in the C# compiler toolchain. This is
+    used to avoid the circular reference in the `Type` class.
     """
 
+    def __init__(self, name: str, parent: Optional[Self] = None) -> None:
+        self._name: str = name
+        self._parent: Optional[Self] = parent
+        self._namespaces: MutableMapping[str, Self] = dict()
+
     def name(self) -> str:
-        raise NotImplementedError
+        return self._name
 
     def parent(self) -> Optional[Self]:
-        raise NotImplementedError
+        return self._parent
 
     def namespaces(self) -> Mapping[str, Self]:
-        raise NotImplementedError
+        return self._namespaces
 
-    def symbols(self):
-        raise NotImplementedError
+    def add_namespace(self, namespace: Self) -> None:
+        self._namespaces[namespace.name()] = namespace
+        namespace._parent = self
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Namespace):
+            return False
+
+        return self._name == value._name and self._parent == value._parent
+
+    def __hash__(self) -> int:
+        return hash(self._name) ^ (hash(self._parent) if self._parent else 0)
 
 
-class Type(_TypeBase):
+class Type:
     """
     Represents a C# type in the compiler toolchain. It can have fields
     which are actual members (instance or static) or nested types. C#
@@ -33,14 +44,23 @@ class Type(_TypeBase):
     """
 
     def __init__(
-        self, name: str, superclass: Optional[Self] = None, interfaces: Optional[Set[Self]] = None
+        self,
+        name: str,
+        namespace: _NamespaceBase,
+        superclass: Optional[Self] = None,
+        interfaces: Optional[Set[Self]] = None,
     ) -> None:
+        """
+        This should not be used directly, but rather through the
+        `Namespace` class which will create the type and add it to the
+        namespace.
+        """
         self._name: str = name
-        self._namespace: _NamespaceBase = None  # This will be set by the Namespace class
+        self._namespace: _NamespaceBase = namespace
         self._fields: MutableMapping[str, Self] = dict()
+        self._superclass: Optional[Self] = superclass
         # Note: This requires Python 3.10+ for set() to be ordered
         self._interfaces: MutableSet[Self] = interfaces if interfaces is not None else set()
-        self._superclass: Optional[Self] = superclass
 
     def name(self) -> str:
         return self._name
@@ -67,55 +87,32 @@ class Type(_TypeBase):
         if not isinstance(value, Type):
             return False
 
-        return self._name == value._name
+        return self._name == value._name and self._namespace == value._namespace
 
     def __hash__(self) -> int:
         return hash(self._name)
 
 
-class Symbols:
-    """
-    Represents the symbols in a C# namespace in the compiler toolchain.
-    """
-
-    def __init__(self, namespace: Namespace) -> None:
-        self._namespace: Namespace = namespace
-        self._types: MutableMapping[str, Type] = dict()
-
-    def add_type(self, type_: Type) -> None:
-        self._types[type_.name()] = type_
-
-    def types(self) -> Mapping[str, Type]:
-        return self._types
-
-
-class Namespace:
+class Namespace(_NamespaceBase):
     """
     Represents a namespace in C# in the compiler toolchain. A namespace can
     have any number of child namespaces and can contain symbols.
     """
 
     def __init__(self, name: str, parent: Optional[Self] = None) -> None:
-        self._name: str = name
-        self._parent: Optional[Self] = parent
-        self._symbols: Symbols = Symbols(self)
-        self._namespaces: MutableMapping[str, Self] = dict()
+        super().__init__(name, parent)
+        self._types: MutableSet[Type] = set()
 
-    def name(self) -> str:
-        return self._name
+    def types(self) -> Set[Type]:
+        return set(self._types)
 
-    def parent(self) -> Optional[Self]:
-        return self._parent
+    def add_type(
+        self, name: str, superclass: Optional[Type] = None, interfaces: Optional[Set[Type]] = None
+    ) -> Type:
+        type_ = Type(name=name, namespace=self, superclass=superclass, interfaces=interfaces)
+        self._types.add(type_)
 
-    def add_namespace(self, namespace: Self) -> None:
-        self._namespaces[namespace.name()] = namespace
-        namespace._parent = self
-
-    def namespaces(self) -> Mapping[str, Self]:
-        return self._namespaces
-
-    def symbols(self) -> Symbols:
-        return self._symbols
+        return type_
 
 
 class Assembly:
@@ -153,3 +150,84 @@ class Assembly:
                     )
 
             current_parent.add_namespace(namespace)
+
+    def get_namespace(self, namespace: str) -> Optional[Namespace]:
+        """
+        Checks if the assembly has a namespace with the given name. If it does,
+        it returns the namespace. If it does not, it returns None. The
+        namespace should be a dot-separated string, e.g.
+        "System.Collections.Generic".
+        """
+        parts: Sequence[str] = namespace.split(".")
+        current_namespace: Optional[Namespace] = self._root
+
+        for part in parts:
+            current_namespace = current_namespace.namespaces().get(part)
+
+            if not current_namespace:
+                return None
+
+        return current_namespace
+
+
+class AssemblyBuilder:
+    def __init__(self, name: str) -> None:
+        self._assembly: Assembly = Assembly(name=name)
+
+    def assembly(self) -> Assembly:
+        return self._assembly
+
+    def add_namespace(self, namespace: str) -> Namespace:
+        """
+        Adds a namespace to the assembly and returns it. The namespace should
+        be a dot-separated string, e.g. "System.Collections.Generic". If the
+        namespace exists, it will return the existing namespace.
+        """
+        if not namespace:
+            raise ValueError("Namespace cannot be empty.")
+
+        ns: Optional[Namespace] = self._assembly.get_namespace(namespace)
+
+        if ns:
+            return ns
+
+        parts: Sequence[str] = namespace.split(".")
+
+        if len(parts) == 1:
+            new_namespace: Namespace = Namespace(name=parts[0], parent=None)
+            self._assembly.add_namespace(namespace=new_namespace, parents=[])
+
+            return new_namespace
+
+        new_namespace: Namespace = Namespace(name=parts[-1], parent=None)
+        parents: Sequence[str] = parts[:-1]
+
+        self._assembly.add_namespace(namespace=new_namespace, parents=parents)
+
+        return new_namespace
+
+    def add_type(
+        self,
+        qname: str,
+        superclass: Optional[Type] = None,
+        interfaces: Optional[Set[Type]] = None,
+    ) -> Type:
+        """
+        Adds a type to the assembly in the given namespace and returns it. If
+        the namespace does not exist, it will be created.
+        """
+        if not qname:
+            raise ValueError("Qualified type name cannot be empty.")
+
+        parts: Sequence[str] = qname.split(".")
+
+        if len(parts) < 2:
+            raise ValueError("Type name must be qualified with a namespace.")
+
+        type_name: str = parts[-1]
+        namespace_name: str = ".".join(parts[:-1])
+
+        ns: Namespace = self.add_namespace(namespace=namespace_name)
+        type_ = ns.add_type(name=type_name, superclass=superclass, interfaces=interfaces)
+
+        return type_
