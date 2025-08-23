cda731ca998767d4da4ce3b7e68329a3f263d4ec
antonsynd <nguyen.antony@gmail.com>
2025-08-23 18:55:00 -0400

    keyword: Arrange alphabetically

--- rust/src/lexer/keyword.rs
+++ rust/src/lexer/keyword.rs
@@ -14,51 +14,51 @@ impl KeywordMap {
         let mut soft_keywords = HashMap::new();
 
         // Hard keywords - these are always reserved
-        keywords.insert("False", TokenType::False);
-        keywords.insert("True", TokenType::True);
-        keywords.insert("None", TokenType::None);
         keywords.insert("and", TokenType::And);
-        keywords.insert("or", TokenType::Or);
-        keywords.insert("not", TokenType::Not);
-        keywords.insert("class", TokenType::Class);
-        keywords.insert("struct", TokenType::Struct);
-        keywords.insert("protocol", TokenType::Protocol);
-        keywords.insert("property", TokenType::Property);
-        keywords.insert("def", TokenType::Def);
-        keywords.insert("return", TokenType::Return);
-        keywords.insert("if", TokenType::If);
-        keywords.insert("else", TokenType::Else);
-        keywords.insert("elif", TokenType::Elif);
-        keywords.insert("for", TokenType::For);
-        keywords.insert("while", TokenType::While);
-        keywords.insert("break", TokenType::Break);
-        keywords.insert("continue", TokenType::Continue);
-        keywords.insert("pass", TokenType::Pass);
-        keywords.insert("try", TokenType::Try);
-        keywords.insert("except", TokenType::Except);
-        keywords.insert("finally", TokenType::Finally);
-        keywords.insert("raise", TokenType::Raise);
-        keywords.insert("import", TokenType::Import);
-        keywords.insert("from", TokenType::From);
         keywords.insert("as", TokenType::As);
+        keywords.insert("assert", TokenType::Assert);
         keywords.insert("async", TokenType::Async);
         keywords.insert("await", TokenType::Await);
-        keywords.insert("assert", TokenType::Assert);
+        keywords.insert("break", TokenType::Break);
+        keywords.insert("class", TokenType::Class);
+        keywords.insert("continue", TokenType::Continue);
+        keywords.insert("def", TokenType::Def);
         keywords.insert("del", TokenType::Del);
-        keywords.insert("with", TokenType::With);
-        keywords.insert("yield", TokenType::Yield);
+        keywords.insert("elif", TokenType::Elif);
+        keywords.insert("else", TokenType::Else);
+        keywords.insert("except", TokenType::Except);
+        keywords.insert("False", TokenType::False);
+        keywords.insert("finally", TokenType::Finally);
+        keywords.insert("for", TokenType::For);
+        keywords.insert("from", TokenType::From);
+        keywords.insert("if", TokenType::If);
         keywords.insert("in", TokenType::In);
         keywords.insert("is", TokenType::Is);
+        keywords.insert("import", TokenType::Import);
         keywords.insert("lambda", TokenType::Lambda);
+        keywords.insert("None", TokenType::None);
+        keywords.insert("not", TokenType::Not);
+        keywords.insert("or", TokenType::Or);
+        keywords.insert("pass", TokenType::Pass);
+        keywords.insert("property", TokenType::Property);
+        keywords.insert("protocol", TokenType::Protocol);
+        keywords.insert("raise", TokenType::Raise);
+        keywords.insert("return", TokenType::Return);
+        keywords.insert("struct", TokenType::Struct);
+        keywords.insert("True", TokenType::True);
+        keywords.insert("try", TokenType::Try);
+        keywords.insert("while", TokenType::While);
+        keywords.insert("with", TokenType::With);
+        keywords.insert("yield", TokenType::Yield);
 
         // Soft keywords - context dependent
-        soft_keywords.insert("type", TokenType::Type);
-        soft_keywords.insert("match", TokenType::Match);
-        soft_keywords.insert("case", TokenType::Case);
-        soft_keywords.insert("get", TokenType::Get);
-        soft_keywords.insert("set", TokenType::Set);
-        soft_keywords.insert("event", TokenType::Event);
         soft_keywords.insert("_", TokenType::Wildcard);
+        soft_keywords.insert("case", TokenType::Case);
+        soft_keywords.insert("event", TokenType::Event);
+        soft_keywords.insert("get", TokenType::Get);
+        soft_keywords.insert("match", TokenType::Match);
+        soft_keywords.insert("set", TokenType::Set);
+        soft_keywords.insert("type", TokenType::Type);
 
         Self {
             keywords,
