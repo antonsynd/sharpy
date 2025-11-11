63069274ef0c1fbbec78b715de8a42e1ff10e092
antonsynd <nguyen.antony@gmail.com>
2025-11-10 23:26:12 -0500

    LexerTests: Format

--- src/Sharpy.Compiler.Tests/Lexer/LexerTests.cs
+++ src/Sharpy.Compiler.Tests/Lexer/LexerTests.cs
@@ -125,7 +125,7 @@ public class LexerTests
     // Note: According to the v0.5 spec, hyphens are not allowed in identifiers.
     // The current lexer behavior is correct: it tokenizes "my-var" as "my", "-", "var".
     // This is not a bug, but proper tokenization.
-    
+
     [Fact]
     public void Tokenize_IdentifierWithHyphen_TokenizesAsThreeSeparateTokens()
     {
@@ -133,7 +133,7 @@ public class LexerTests
         // so "my-var" is tokenized as identifier "my", minus operator "-", and identifier "var"
         var source = "my-var";
         var tokens = Tokenize(source);
-        
+
         // Should have: identifier("my"), minus("-"), identifier("var"), EOF
         tokens.Should().HaveCount(4);
         tokens[0].Type.Should().Be(TokenType.Identifier);
