c2b9fad50274a413ae300d11e10eb39ac2e4d457
antonsynd <nguyen.antony@gmail.com>
2026-03-14 00:46:18 -0400

    refactor(validation): migrate AccessValidator to ValidatingAstWalker
    Replace manual ValidateTopLevelStatement/ValidateStatement/ValidateExpression
    switches with AstVisitor traversal. Override VisitClassDef/VisitStructDef for
    scope tracking and VisitMemberAccess for access checks.
    
    Fix generator_basic_0014 test fixture: use public get_mode() accessor instead
    of direct protected _mode access from module level, since the more complete
    AstVisitor traversal now correctly detects this access violation in f-strings.

--- src/Sharpy.Compiler/Semantic/Validation/AccessValidator.cs
+++ src/Sharpy.Compiler/Semantic/Validation/AccessValidator.cs
@@ -15,253 +15,48 @@ namespace Sharpy.Compiler.Semantic.Validation;
 /// Unlike the legacy version which is called during expression type-checking,
 /// this validator performs a post-pass over the AST.
 /// </summary>
-internal class AccessValidator : SemanticValidatorBase
+internal class AccessValidator : ValidatingAstWalker
 {
     public override string Name => "AccessValidator";
     public override int Order => 450; // After control flow (400)
 
     private ICompilerLogger _logger = NullLogger.Instance;
-    private SemanticContext _context = null!;
 
     public override void Validate(Module module, SemanticContext context)
     {
-        _context = context;
         _logger = context.Logger;
         _logger.LogDebug("Starting access validation");
+        base.Validate(module, context);
+    }
 
-        foreach (var stmt in module.Body)
+    public override void VisitClassDef(ClassDef node)
+    {
+        var classSymbol = Context.SymbolTable.LookupType(node.Name);
+        using (Context.Traversal.EnterClass(classSymbol))
         {
-            ValidateTopLevelStatement(stmt);
+            base.VisitClassDef(node);
         }
     }
 
-    private void ValidateTopLevelStatement(Statement stmt)
+    public override void VisitStructDef(StructDef node)
     {
-        switch (stmt)
+        var structSymbol = Context.SymbolTable.LookupType(node.Name);
+        using (Context.Traversal.EnterClass(structSymbol))
         {
-            case FunctionDef funcDef:
-                ValidateFunction(funcDef);
-                break;
-            case ClassDef classDef:
-                ValidateClass(classDef);
-                break;
-            case StructDef structDef:
-                ValidateStruct(structDef);
-                break;
-            case ExpressionStatement exprStmt:
-                ValidateExpression(exprStmt.Expression);
-                break;
-            case Assignment assignment:
-                ValidateExpression(assignment.Target);
-                ValidateExpression(assignment.Value);
-                break;
-            case VariableDeclaration varDecl:
-                if (varDecl.InitialValue != null)
-                    ValidateExpression(varDecl.InitialValue);
-                break;
+            base.VisitStructDef(node);
         }
     }
 
-    private void ValidateClass(ClassDef classDef)
+    public override void VisitMemberAccess(MemberAccess node)
     {
-        var classSymbol = _context.SymbolTable.LookupType(classDef.Name);
-        using (_context.Traversal.EnterClass(classSymbol))
-        {
-            foreach (var member in classDef.Body)
-            {
-                ValidateStatement(member);
-            }
-        }
-    }
-
-    private void ValidateStruct(StructDef structDef)
-    {
-        var structSymbol = _context.SymbolTable.LookupType(structDef.Name);
-        using (_context.Traversal.EnterClass(structSymbol))
-        {
-            foreach (var member in structDef.Body)
-            {
-                ValidateStatement(member);
-            }
-        }
-    }
-
-    private void ValidateFunction(FunctionDef funcDef)
-    {
-        foreach (var stmt in funcDef.Body)
-        {
-            ValidateStatement(stmt);
-        }
-    }
-
-    private void ValidateStatement(Statement stmt)
-    {
-        switch (stmt)
-        {
-            case FunctionDef funcDef:
-                ValidateFunction(funcDef);
-                break;
-            case ExpressionStatement exprStmt:
-                ValidateExpression(exprStmt.Expression);
-                break;
-            case Assignment assignment:
-                ValidateExpression(assignment.Target);
-                ValidateExpression(assignment.Value);
-                break;
-            case VariableDeclaration varDecl:
-                if (varDecl.InitialValue != null)
-                    ValidateExpression(varDecl.InitialValue);
-                break;
-            case ReturnStatement returnStmt:
-                if (returnStmt.Value != null)
-                    ValidateExpression(returnStmt.Value);
-                break;
-            case IfStatement ifStmt:
-                ValidateExpression(ifStmt.Test);
-                foreach (var s in ifStmt.ThenBody)
-                    ValidateStatement(s);
-                foreach (var elif in ifStmt.ElifClauses)
-                {
-                    ValidateExpression(elif.Test);
-                    foreach (var s in elif.Body)
-                        ValidateStatement(s);
-                }
-                foreach (var s in ifStmt.ElseBody)
-                    ValidateStatement(s);
-                break;
-            case WhileStatement whileStmt:
-                ValidateExpression(whileStmt.Test);
-                foreach (var s in whileStmt.Body)
-                    ValidateStatement(s);
-                break;
-            case ForStatement forStmt:
-                ValidateExpression(forStmt.Iterator);
-                foreach (var s in forStmt.Body)
-                    ValidateStatement(s);
-                break;
-            case TryStatement tryStmt:
-                foreach (var s in tryStmt.Body)
-                    ValidateStatement(s);
-                foreach (var handler in tryStmt.Handlers)
-                {
-                    foreach (var s in handler.Body)
-                        ValidateStatement(s);
-                }
-                foreach (var s in tryStmt.ElseBody)
-                    ValidateStatement(s);
-                foreach (var s in tryStmt.FinallyBody)
-                    ValidateStatement(s);
-                break;
-            case WithStatement withStmt:
-                foreach (var item in withStmt.Items)
-                    ValidateExpression(item.ContextExpression);
-                foreach (var s in withStmt.Body)
-                    ValidateStatement(s);
-                break;
-            case RaiseStatement raiseStmt:
-                if (raiseStmt.Exception != null)
-                    ValidateExpression(raiseStmt.Exception);
-                break;
-            case AssertStatement assertStmt:
-                ValidateExpression(assertStmt.Test);
-                if (assertStmt.Message != null)
-                    ValidateExpression(assertStmt.Message);
-                break;
-        }
-    }
-
-    private void ValidateExpression(Expression expr)
-    {
-        switch (expr)
-        {
-            case MemberAccess memberAccess:
-                ValidateMemberAccess(memberAccess);
-                ValidateExpression(memberAccess.Object);
-                break;
-            case FunctionCall call:
-                ValidateExpression(call.Function);
-                foreach (var arg in call.Arguments)
-                    ValidateExpression(arg);
-                foreach (var kwArg in call.KeywordArguments)
-                    ValidateExpression(kwArg.Value);
-                break;
-            case BinaryOp binOp:
-                ValidateExpression(binOp.Left);
-                ValidateExpression(binOp.Right);
-                break;
-            case UnaryOp unaryOp:
-                ValidateExpression(unaryOp.Operand);
-                break;
-            case IndexAccess indexAccess:
-                ValidateExpression(indexAccess.Object);
-                ValidateExpression(indexAccess.Index);
-                break;
-            case ListLiteral listLit:
-                foreach (var elem in listLit.Elements)
-                    ValidateExpression(elem);
-                break;
-            case DictLiteral dictLit:
-                foreach (var entry in dictLit.Entries)
-                {
-                    if (entry.Key != null)
-                        ValidateExpression(entry.Key);
-                    ValidateExpression(entry.Value);
-                }
-                break;
-            case SetLiteral setLit:
-                foreach (var elem in setLit.Elements)
-                    ValidateExpression(elem);
-                break;
-            case TupleLiteral tupleLit:
-                foreach (var elem in tupleLit.Elements)
-                    ValidateExpression(elem);
-                break;
-            case ConditionalExpression cond:
-                ValidateExpression(cond.Test);
-                ValidateExpression(cond.ThenValue);
-                ValidateExpression(cond.ElseValue);
-                break;
-            case ListComprehension listComp:
-                ValidateExpression(listComp.Element);
-                foreach (var clause in listComp.Clauses)
-                {
-                    if (clause is ForClause forClause)
-                        ValidateExpression(forClause.Iterator);
-                    else if (clause is IfClause ifClause)
-                        ValidateExpression(ifClause.Condition);
-                }
-                break;
-            case SetComprehension setComp:
-                ValidateExpression(setComp.Element);
-                foreach (var clause in setComp.Clauses)
-                {
-                    if (clause is ForClause forClause)
-                        ValidateExpression(forClause.Iterator);
-                    else if (clause is IfClause ifClause)
-                        ValidateExpression(ifClause.Condition);
-                }
-                break;
-            case DictComprehension dictComp:
-                ValidateExpression(dictComp.Key);
-                ValidateExpression(dictComp.Value);
-                foreach (var clause in dictComp.Clauses)
-                {
-                    if (clause is ForClause forClause)
-                        ValidateExpression(forClause.Iterator);
-                    else if (clause is IfClause ifClause)
-                        ValidateExpression(ifClause.Condition);
-                }
-                break;
-            case Parenthesized paren:
-                ValidateExpression(paren.Expression);
-                break;
-        }
+        ValidateMemberAccess(node);
+        base.VisitMemberAccess(node);
     }
 
     private void ValidateMemberAccess(MemberAccess memberAccess)
     {
         // Get the type of the object being accessed
-        var objectType = _context.SemanticInfo.GetExpressionType(memberAccess.Object);
+        var objectType = Context.SemanticInfo.GetExpressionType(memberAccess.Object);
         if (objectType == null)
             return;
 
@@ -269,7 +64,7 @@ internal class AccessValidator : SemanticValidatorBase
         TypeSymbol? owningType = null;
         if (objectType is UserDefinedType udt)
         {
-            owningType = udt.Symbol ?? _context.SymbolTable.LookupType(udt.Name);
+            owningType = udt.Symbol ?? Context.SymbolTable.LookupType(udt.Name);
         }
 
         if (owningType == null)
@@ -288,9 +83,9 @@ internal class AccessValidator : SemanticValidatorBase
         {
             case AccessLevel.Private:
                 // Private members only accessible within the same class
-                if (_context.Traversal.CurrentClass != owningType)
+                if (Context.Traversal.CurrentClass != owningType)
                 {
-                    AddError(_context,
+                    AddError(
                         $"Cannot access private member '{memberName}' of '{owningType.Name}' from outside the class",
                         lineStart, columnStart, code: DiagnosticCodes.Semantic.AccessViolation,
                         span: span);
@@ -299,9 +94,9 @@ internal class AccessValidator : SemanticValidatorBase
 
             case AccessLevel.Protected:
                 // Protected members accessible within the class hierarchy
-                if (_context.Traversal.CurrentClass == null || !IsInHierarchy(_context.Traversal.CurrentClass, owningType))
+                if (Context.Traversal.CurrentClass == null || !IsInHierarchy(Context.Traversal.CurrentClass, owningType))
                 {
-                    AddError(_context,
+                    AddError(
                         $"Cannot access protected member '{memberName}' of '{owningType.Name}' from outside the class hierarchy",
                         lineStart, columnStart, code: DiagnosticCodes.Semantic.AccessViolation,
                         span: span);
@@ -341,14 +136,14 @@ internal class AccessValidator : SemanticValidatorBase
             return true;
 
         // Check if currentClass is a subclass of targetClass
-        foreach (var ancestor in TypeHierarchyService.GetAllBaseTypes(currentClass, _context.SemanticBinding))
+        foreach (var ancestor in TypeHierarchyService.GetAllBaseTypes(currentClass, Context.SemanticBinding))
         {
             if (ReferenceEquals(ancestor, targetClass) || ancestor.Name == targetClass.Name)
                 return true;
         }
 
         // Check if targetClass is a subclass of currentClass
-        foreach (var ancestor in TypeHierarchyService.GetAllBaseTypes(targetClass, _context.SemanticBinding))
+        foreach (var ancestor in TypeHierarchyService.GetAllBaseTypes(targetClass, Context.SemanticBinding))
         {
             if (ReferenceEquals(ancestor, currentClass) || ancestor.Name == currentClass.Name)
                 return true;
